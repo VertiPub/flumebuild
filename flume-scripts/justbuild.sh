c8897f3652379ab474ac957c906b921a01c28378
Lee T. Tucker <ltucker@verticloud.com>
2013-02-13 15:48:26 -0800

    Make the mvn run in a local .m2 dir with no archiva repo

--- flume-scripts/justbuild.sh
+++ flume-scripts/justbuild.sh
@@ -1,2 +1,4 @@
 #!/bin/sh -ex
-mvn clean install -DskipTests
+mkdir -p ${WORKSPACE}/.m2
+echo "<settings></settings>" > ${WORKSPACE}/.m2/settings.xml
+mvn clean install -DskipTests -Dmaven.repo.local=${WORKSPACE}/.m2 --settings=${WORKSPACE}/.m2/settings.xml
